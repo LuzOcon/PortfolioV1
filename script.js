ec30259dd7e6b5365e80191bea9d17f359b62d12
Luz Ocon <luzhdez711@gmail.com>
2025-10-10 18:45:23 -0600

    Added new sections and individual project pages

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
 document.addEventListener("scroll", function() {
   const navbar = document.querySelector(".navbar");
+  const dropdownmenu = document.querySelector(".dropdown-menu");
   if (window.scrollY > 50) {
     navbar.classList.add("scrolled"); // agrega clase scrolled
+    dropdownmenu.classList.add("scrolled");
   } else {
     navbar.classList.remove("scrolled"); // quita clase scrolled
+    dropdownmenu.classList.remove("scrolled");
   }
 });
+
+function filtrarProyectos(categoria) {
+  const proyectos = document.querySelectorAll('.proyecto');
+  for (let i = 0; i < proyectos.length; i++) {
+    const p = proyectos[i];
+    if (categoria === 'todos') {
+      p.style.display = 'block';
+    } else if (p.classList.contains(categoria)) {
+      p.style.display = 'block';
+    } else {
+      p.style.display = 'none';
+    }
+  }
+}
+    
+function aplicarFiltroHash() {
+  let categoria = location.hash.replace('#','');
+  if (!categoria || categoria === '') {
+    categoria = 'todos';
+  }
+
+  filtrarProyectos(categoria);
+}
+
+
+window.onload = function() {
+  aplicarFiltroHash();
+};
+
+window.onhashchange = function() {
+  aplicarFiltroHash();
+};
